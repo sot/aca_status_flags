d4e047f0ce63ae0c49b5ca65e4518a9aa7d58a14
Tom Aldcroft <taldcroft@gmail.com>
2013-07-15 12:26:42 -0400

    Update to use different slots

--- plot_flags.py
+++ plot_flags.py
@@ -18,8 +18,7 @@ pcad_msids = ['aoacfct',
               'aoaczan',
               'aoimage']
 
-
-slots = range(0, 1)
+slots = [6]
 slot_msids = [msid + "%s" % slot for slot in slots for msid in pcad_msids]
 
 msids = ['aopcadmd',
@@ -31,12 +30,14 @@ msids = ['aopcadmd',
 
 msids.extend(slot_msids)
 
-obsid = '56345'
+obsid = 15304  # 95 second drop on 2013:104:23:40:08.  See also obsid 56345.
+obsid = 15175
+obsid = 56345
 
-db = Ska.DBI.DBI(dbi='sybase', server='sybase')
+db = Ska.DBI.DBI(dbi='sybase', server='sybase', user='aca_read')
 if 'obs' not in globals():
     obs = db.fetchall("""select obsid, kalman_datestart, kalman_datestop
-    from observations where obsid = %s""" % obsid)[0]
+    from observations where obsid = {}""".format(obsid))[0]
 
 if 'telem' not in globals():
     telem = fetch.MSIDset(msids,
@@ -44,17 +45,19 @@ if 'telem' not in globals():
                           obs['kalman_datestop'],
                           filter_bad=True,
                           )
+telem.interpolate(dt=2.05)
 
-vals = [telem['aoattqt%d' % i].vals for i in range(1, 5)] + [telem['aoacyan0'].vals / 3600.,
-                                                             telem['aoaczan0'].vals / 3600.]
+slot = 6
+vals = ([telem['aoattqt%d' % i].vals for i in range(1, 5)]
+        + [telem['aoacyan{}'.format(slot)].vals / 3600.,
+           telem['aoaczan{}'.format(slot)].vals / 3600.])
 if 'coords' not in globals():
     radecs = [yagzag2radec(yag, zag, Quat([q1, q2, q3, q4]))
               for q1, q2, q3, q4, yag, zag in zip(*vals)]
     coords = np.rec.fromrecords(radecs, names=('ra', 'dec'))
 
 
-ok = telem['aoacfct0'].vals == 'TRAK'
-slot = 0
+ok = telem['aoacfct{}'.format(slot)].vals == 'TRAK'
 
 flags = {'dp': telem['aoacidp%s' % slot].vals != 'OK ',
          'ir': telem['aoaciir%s' % slot].vals != 'OK ',
@@ -70,12 +73,12 @@ color = {'dp': 'blue',
 
 
 def plot_axis(label, dy, ok, dp, ir, ms, sp, filename=None):
-    plt.figure(1, figsize=(4, 2.5))
+    plt.figure(figsize=(6, 4))
     plt.clf()
-    filt = ok & (flags['dp'] == dp) & (flags[
-                                       'ir'] == ir) & (flags['ms'] == ms) & (flags['sp'] == sp)
+    filt = (ok & (flags['dp'] == dp) & (flags['ir'] == ir)
+            & (flags['ms'] == ms) & (flags['sp'] == sp))
 
-    plot_cxctime(times[ok], dy[ok], 'r.', markersize=.3)
+    plot_cxctime(times[ok], dy[ok], 'r.', markersize=0.3)
     if len(np.flatnonzero(filt)) > 0:
         plot_cxctime(times[filt], dy[filt], 'k.', markersize=2)
     plt.ylabel('Delta %s (arcsec)' % label)
@@ -95,7 +98,7 @@ for dp in (False, True):
     for ir in (False, True):
         for ms in (False, True):
             for sp in (False, True):
-                plot_axis('RA', dra, ok, dp, ir, ms, sp, 'flags_ra_')
+                plot_axis('RA', dra, ok, dp, ir, ms, sp, '{}/flags_ra_'.format(obsid))
 
 
 # plot_axis('Dec', ddec, 'delta_dec.png')
